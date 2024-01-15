2e4d01ba6113ce5a4d689a0c7f5b3c2d819a66c7
Артём Ярославцев <gr414_yaravya@lab-402-14.tomtit-tomsk.ru>
2024-01-15 11:26:50 +0700

    practical 7

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of socks",
         inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
@@ -11,21 +12,39 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
-        cart: 0
+        cart: 0,
+        onSale: true,
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            return this.onSale ? `${this.brand} ${this.product} is currently on sale` : ''
         }
     }
 })
