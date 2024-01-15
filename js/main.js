27397de3342fe96d8adcefe2914cc9511a9892eb
Артём Ярославцев <gr414_yaravya@lab-402-14.tomtit-tomsk.ru>
2024-01-15 11:42:32 +0700

    practical 8

--- js/main.js
+++ js/main.js
@@ -1,50 +1,107 @@
+Vue.component("product-details", {
+  props: {
+    details: {
+      type: Array,
+      required: true,
+    },
+  },
+  template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    `,
+});
+
+Vue.component("product", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template: `
+        <div class="product">
+
+            <div class="product-image">
+                <img :src="image" :alt="altText" />
+            </div>
+
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p v-if="inStock">In stock</p>
+                <p v-else>Out of Stock</p>
+
+                <product-details :details="details"/>
+
+                <p>Shipping: {{ shipping }}</p>
+
+                <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId"
+                    :style="{ backgroundColor:variant.variantColor }" @mouseover="updateProduct(index)"></div>
+            </div>
+
+            <div class="cart">
+                <p>Cart({{ cart }})</p>
+            </div>
+
+            <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
+                Add to cart
+            </button>
+        </div>
+    `,
+  data: () => ({
+    product: "Socks",
+    brand: "Vue Mastery",
+    selectedVariant: 0,
+    altText: "A pair of socks",
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10,
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0,
+      },
+    ],
+    cart: 0,
+  }),
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      } else {
+        return 2.99;
+      }
+    },
+  },
+});
+
 let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        inStock: false,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0
-            }
-        ],
-        cart: 0,
-        onSale: true,
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-            console.log(index);
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-        sale() {
-            return this.onSale ? `${this.brand} ${this.product} is currently on sale` : ''
-        }
-    }
-})
+  el: "#app",
+  data: {
+    premium: true,
+  },
+});
