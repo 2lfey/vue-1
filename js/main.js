7aebe09b8dfc4c30596d7e62b0d9f19c46af7f43
ilfey <ilfey.global@gmail.com>
2024-01-15 17:01:40 +0300

    practical 9

--- js/main.js
+++ js/main.js
@@ -1,80 +1,90 @@
-Vue.component("product-details", {
-  props: {
-    details: {
-      type: Array,
-      required: true,
-    },
-  },
-  template: `
-        <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-    `,
-});
-
 Vue.component("product", {
   props: {
     premium: {
       type: Boolean,
       required: true,
     },
+    canRemoveFromCard: {
+      type: Boolean,
+      required: true,
+    },
   },
   template: `
-        <div class="product">
+   <div class="product">
+    <div class="product-image">
+           <img :src="image" :alt="altText"/>
+       </div>
 
-            <div class="product-image">
-                <img :src="image" :alt="altText" />
-            </div>
+       <div class="product-info">
+           <h1>{{ title }}</h1>
+           <p v-if="inStock">In stock</p>
+           <p v-else>Out of Stock</p>
+           <ul>
+               <li v-for="detail in details">{{ detail }}</li>
+           </ul>
+          <p>Shipping: {{ shipping }}</p>
+           <div
+                class="color-box"
+                v-for="(variant, index) in variants"
+                :key="variant.variantId"
+                :style="{ backgroundColor:variant.variantColor }"
+                @mouseover="updateProduct(index)"
+           ></div>
 
-            <div class="product-info">
-                <h1>{{ title }}</h1>
-                <p v-if="inStock">In stock</p>
-                <p v-else>Out of Stock</p>
+           <button
+                v-on:click="removeFromCart"
+                :disabled="!canRemoveFromCard"
+                :class="{ disabledButton: !canRemoveFromCard }"
+           >
+               Remove from cart
+           </button>
 
-                <product-details :details="details"/>
-
-                <p>Shipping: {{ shipping }}</p>
-
-                <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId"
-                    :style="{ backgroundColor:variant.variantColor }" @mouseover="updateProduct(index)"></div>
-            </div>
-
-            <div class="cart">
-                <p>Cart({{ cart }})</p>
-            </div>
-
-            <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
-                Add to cart
-            </button>
-        </div>
-    `,
-  data: () => ({
-    product: "Socks",
-    brand: "Vue Mastery",
-    selectedVariant: 0,
-    altText: "A pair of socks",
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "green",
-        variantImage: "./assets/vmSocks-green-onWhite.jpg",
-        variantQuantity: 10,
-      },
-      {
-        variantId: 2235,
-        variantColor: "blue",
-        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-        variantQuantity: 0,
-      },
-    ],
-    cart: 0,
-  }),
+           <button
+                v-on:click="addToCart"
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock }"
+           >
+               Add to cart
+           </button>
+       
+       </div>
+   </div>
+ `,
+  data() {
+    return {
+      product: "Socks",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
+      altText: "A pair of socks",
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage: "./assets/vmSocks-green-onWhite.jpg",
+          variantQuantity: 10,
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+          variantQuantity: 0,
+        },
+      ],
+    };
+  },
   methods: {
     addToCart() {
-      this.cart += 1;
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].variantId);
+    },
+    removeFromCart() {
+      this.$emit(
+        "remove-from-cart",
+        this.variants[this.selectedVariant].variantId
+      );
     },
     updateProduct(index) {
+      this.$emit("update-current-product-id", this.variants[index].variantId);
       this.selectedVariant = index;
       console.log(index);
     },
@@ -103,5 +113,37 @@ let app = new Vue({
   el: "#app",
   data: {
     premium: true,
+    cart: [],
+    // canRemoveFromCard: false,
+    _updateCurrentProductId: -1,
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
+    },
+    updateCurrentProductId(id) {
+      this._updateCurrentProductId = id;
+      console.log(id);
+      this.canRemoveFromCard =
+        this.cart.length > 0 &&
+        this.cart.filter((i) => i.variantId === id).length > 0;
+      console.log(this.canRemoveFromCard);
+    },
+
+    removeProduct(id) {
+      let index = this.cart.indexOf(id);
+      if (index > -1) {
+        this.cart.splice(index, 1);
+      }
+    },
+  },
+  computed: {
+    canRemoveFromCard() {
+      return (
+        this.cart.length > 0 &&
+        this.cart.filter((i) => i.variantId === this._updateCurrentProductId)
+          .length > 0
+      );
+    },
   },
 });
